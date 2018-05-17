16d09e41f3c3ac69a087e935f3ec2798ba918cf7
Nikos Katsilidis <nikos.katsilidis@gmail.com>
2018-05-17 22:49:47 +0300

    finished
    finished

--- security_3/src/Main.java
+++ security_3/src/Main.java
@@ -1,22 +1,19 @@
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
+/**
+ *
+ * icsd14071 Karadimos Michail
+ * icsd14081 Katsilidis Nikolaos
+ *
+ * */
+
+import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.*;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.SecureRandom;
+import java.security.*;
 import java.util.Arrays;
-import java.util.Base64;
 import java.util.Scanner;
-import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 
 
 public class Main {
@@ -24,12 +21,6 @@ public class Main {
 
     //cipher text twn 256 byte
     private final static String cipherText4 = "f20bdba6ff29eed7b046d1df9fb7000058b1ffb4210a580f748b4ac714c001bd4a61044426fb515dad3f21f18aa577c0bdf302936266926ff37dbf7035d5eeb4";
-
-    private final static String cipherText3 = "f20bdba6ff29eed7b046d1df9fb7000058b1ffb4210a580f748b4ac714c001bd4a61044426fb515dac3e20f08ba476c0";
-
-    private final static String cipherText2 = "f20bdba6ff29eed7b046d1df9fb7000058b1ffb4210a580f748b4ac714c001bd";
-
-    private final static String cipherText1 = "f20bdba6ff29eed7b046d1df9fb70000";
 
     private final static String decryptedPlainText = "The Magic Words are Squeamish Ossifrage";
 
@@ -67,7 +58,6 @@ public class Main {
         int cipherLenght = cipherText4.length();     //pairnw to mhkos tou cipher pou einai se byte
 
 
-
         ciphers = new byte[cipherLenght / 32][16];    //ftiaxnw pinaka apo byte pou dexetai 16byte cipher texts
         ciphers2 = new byte[cipherLenght / 32][16];    //ftiaxnw pinaka apo byte pou dexetai 16byte cipher texts
         StringBuilder stByte = new StringBuilder();
@@ -75,12 +65,11 @@ public class Main {
         byte cipherByte[] = toByteArray(cipherText4); //metatroph tou cipher apo string se pinaka apo byte mege8ous 64 byte
 
 
-
         //4 epanalhpseis gia th dhmiourgia 4 block ka8e grammh tou pinaka chiper periexei 16 byte chiper
         for (int i = 0; i < 4; i++) {
             ciphers[i] = Arrays.copyOfRange(cipherByte, i * 16, 16 * (i + 1));
             ciphers2[i] = Arrays.copyOfRange(cipherByte, i * 16, 16 * (i + 1));
-            System.out.println(i+1 + ") " +toHexString(ciphers[i]));    //ektupwsh tou pinaka se hex string
+            System.out.println(i + 1 + ") " + toHexString(ciphers[i]));    //ektupwsh tou pinaka se hex string
 
         }
         System.out.println();
@@ -88,17 +77,21 @@ public class Main {
 
     }
 
-    public static int sendRequest(byte ciphers[][]) {
+    public static int sendRequest(byte[][] ciphers, int b) {
         StringBuilder cipher = new StringBuilder();
 
-        for (int i = 0; i < ciphers.length; i++) {
+        //kanei append sto stringbuilder ta block pou xreiazetai ka8e fora na steilei to url gia na parei thn katallhlh apanthsh
+        //px url me 4 block, meta url me 3 block k.o.k
+        //to b einai to block pou briskomai auth th stigmh kai to pairnw apo thn bruteForce
+        //p.x. otan to i = 0 to b = 2 opote ka8e fora allazoun oi epanalhoseis ths for dld 4,3,2
+        for (int i = 0; i < ciphers.length + (b - 2); i++) {
 
             cipher.append(toHexString(ciphers[i]));
 
 
         }
 
-       // System.out.println(cipher);
+        // System.out.println(cipher);
 
         //Connection kai request sto url pou 8eloume
         int code = 0;
@@ -118,9 +111,9 @@ public class Main {
             //epistrefei 403 gia mh egkuro padding (forbidden request) kai 404 gia ekuro padding alla alloiwmeno (URL not found)
 
             if (code == 403) {
-               //System.out.println("Error 403 mh egkuro padding");
+                //System.out.println("Error 403 mh egkuro padding");
             } else if (code == 404) {
-               // System.out.println("Error 404 egkuro padding");
+                // System.out.println("Error 404 egkuro padding");
             }
 
         } catch (MalformedURLException e) {
@@ -134,7 +127,6 @@ public class Main {
     }
 
 
-
     //sunarthsh bruteforce
     public static void bruteForce() {
         Integer x;
@@ -142,59 +134,58 @@ public class Main {
         StringBuilder st1 = new StringBuilder();
 
 
-
-        // cycle for all 16 byte blocks except IV
-        for (int b = 3; b >0; b--) {
-            System.out.println("Block: " + (b+1) );
+        // epanalhpsh gia ola ta block ektos apo to 4
+        for (int b = 2; b >= 0; b--) {
+            System.out.println("Block: " + (b + 1));
 
             int y = 0;
 
-
-
-            // cycle for all paddings
+            // epanalhpsh gia ka8e padding tou block
             for (int t = 15; t >= 0; t--) {
                 System.out.println("pad: " + (t + 1));
 
-
                 for (int k = t + 1; k <= 15; k++) {
-
-                    ciphers[b-1][k] = (byte) (ciphers[b-1][k] ^ y ^ (y + 1));
-
-
+                    ciphers[b][k] = (byte) (ciphers[b][k] ^ y ^ (y + 1));
                 }
                 y++;
+
 
                 //epanalhpsh pou paragei olous tous dunatous sunduasmous apo hex
                 for (int i = 0; i < 256; i++) {
                     x = i;
 
-                    ciphers[b-1][t] = x.byteValue();
+                    ciphers[b][t] = x.byteValue();
 
-                    System.out.println(toHexString(ciphers[b-1]));
+                    System.out.println(toHexString(ciphers[b]));
 
-                    int res = sendRequest(ciphers);
+                    int res = sendRequest(ciphers, b);
 
                     System.out.println(res);
 
+
+                    //elegxoume thn apanthsh tou server sto egkuro alla alloiwmeno padding
                     if (res == 404 || res == 200) {
 
-                        if(res==200 && t==15){ continue; }
 
-                        System.out.println((ciphers[b-1][t] ^ ciphers2[b-1][t] ^ y));
+                        if (res == 200 && t == 15) {
+                            continue;
+                        }
 
-                        st.append((ciphers[b-1][t] ^ ciphers2[b-1][t] ^ y));
+                        System.out.println((ciphers[b][t] ^ ciphers2[b][t] ^ y));
+
+                        st.append((ciphers[b][t] ^ ciphers2[b][t] ^ y));
 
                         System.out.println(st);
                         System.out.println();
 
-                        if ((ciphers[b-1][t] ^ ciphers2[b-1][t] ^ y) > 32) {
-                            st1.append((char) ((ciphers[b-1][t] ^ ciphers2[b-1][t] ^ y)));
+                        //elegxoume an uparxei keno
+                        //to 32 einai to keno sto ascii
+                        if ((ciphers[b][t] ^ ciphers2[b][t] ^ y) > 32) {
+                            st1.append((char) ((ciphers[b][t] ^ ciphers2[b][t] ^ y)));
                             System.out.println(st1);
                             System.out.println();
 
                         }
-
-
 
                         break;
                     }
@@ -205,17 +196,87 @@ public class Main {
 
         }
         System.out.println(st);
-       // System.out.println();
+        // System.out.println();
     }
 
+    //Sunarthsh gia dhmiourgia kleidiou gia ton AES
     public static SecretKey createKeyForAES(
-            @SuppressWarnings("unused") int bitLength, SecureRandom random)
-            throws NoSuchAlgorithmException, NoSuchProviderException {
-        KeyGenerator generator = KeyGenerator.getInstance("AES");
+            @SuppressWarnings("unused") int bitLength, SecureRandom random) {
+        KeyGenerator generator = null;
+        try {
+            generator = KeyGenerator.getInstance("AES");    //Dhlwnoume th typou 8a einai to kleidi p.x. AES
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
 
-        generator.init(256, random);
+        generator.init(bitLength, random);
 
         return generator.generateKey();
+    }
+
+
+    //Sunarthsh gia dhmiourgia kleidiou gia ton HMAC
+    private static Key createKeyForHMAC() {
+        KeyGenerator generator = null;
+        try {
+            generator = KeyGenerator.getInstance("HmacSHA256");   //Dhlwnoume th typou 8a einai to kleidi p.x. HmacSHA256
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+
+        return generator.generateKey();
+    }
+
+    private static byte[] encryptAES(Key key, byte[] message) {
+
+        int ivSize = 16;
+        byte[] iv = new byte[ivSize];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(iv);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+
+        Cipher cipher = null;
+        try {
+            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+            cipher.init(Cipher.ENCRYPT_MODE, key, ivParameterSpec);
+            byte[] encrypted = cipher.doFinal(message);
+            return encrypted;
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    private static byte[] encryptMAC(Key key2, byte[] cipher) {
+        byte[] mac_data = null;
+        try {
+            Mac sha512_HMAC;
+            sha512_HMAC = Mac.getInstance("HmacSHA256");
+
+            SecretKeySpec keySpec = new SecretKeySpec(key2.getEncoded(), "HmacSHA256");
+            sha512_HMAC.init(keySpec);
+
+            mac_data = sha512_HMAC.
+                    doFinal(cipher);
+        }catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+        return mac_data;
     }
 
 
@@ -227,27 +288,40 @@ public class Main {
         System.out.println("2.Create Key.");
         int choice = reader.nextInt();
 
-        switch(choice)
-        {
+        switch (choice) {
             case 1:
-                breakToBlocks();
-
-                bruteForce();
+                breakToBlocks();    //spaei to cipher se 4 block
+                bruteForce();       //epi8esh se ka8e block gia na parw to plaintext
                 break;
 
             case 2:
                 SecureRandom random = new SecureRandom();
 
-                try {
-                    Key key = createKeyForAES(256, random);
-                    System.out.println(key);
+                //dhmiourgia prwtou kleidiou gia ton AES
+                Key key = createKeyForAES(128, random);
+                //System.out.println(toHexString(key.getEncoded()));
 
-                } catch (NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                } catch (NoSuchProviderException e) {
-                    e.printStackTrace();
-                }
+                //dhmiourgia deuterou kleidiou gia ton HMAC
+                Key key2 = createKeyForHMAC();
+                //System.out.println(toHexString(key2.getEncoded()));
 
+                //kanoume encrypt me to prwto kleidi to plaintext
+                byte[] cipher = encryptAES(key, decryptedPlainText.getBytes());
+                System.out.println(toHexString(cipher));
+
+                //kanoume encrypt me to deutero kleidi to cipher gia na paroume to MAC tou
+                byte[] cipher2 = encryptMAC(key2, cipher);
+                System.out.println(toHexString(cipher2));
+
+
+                //ZHTHMA III
+                //Peirazoume to prwto padding tou cipher
+                cipher[0] = 1;
+                System.out.println(toHexString(cipher));
+
+                //kanoume encrypt to cipher gia na paroume to MAC tou
+                byte[] cipher3 = encryptMAC(key2, cipher);
+                System.out.println(toHexString(cipher3));
 
                 break;
 
@@ -256,16 +330,13 @@ public class Main {
                 break;
         }
 
-
-
-
-
-
-
     }
+
+
 
 }
 
 
 // c8 ^ D(b4) = 01   =>  D(b4)  = 01 ^ c8
 // c8' ^ D(b4) = 02  =>  c8' = D(b4) ^02 => c8' = 01 ^ 02 ^ c8 = cb
+
